78bf63baea6ec4591bec1f4386a87ea8603186a2
teauk03 <kto031124@gamil.com>
2024-03-01 18:52:42 +0900

    Inheritance Ex2

--- src/Inheritance/Iphone.java
+++ src/Inheritance/Iphone.java
@@ -10,11 +10,13 @@ class SmartPhone {
         this.PhoneNum = PhoneNum;
         this.PhoneModel = PhoneModel;
     }
+    // 전화 거는 메서드
     public void SmartPhoneFn1 () {
         System.out.println("스마트 폰으로는 전화를 걸 수 있습니다 통신을 시도할 전화번호를 입력하세요.");
         int UserPhoneNum = scanner.nextInt();
         System.out.println(UserPhoneNum + "번호로 전화를 시도합니다.");
     }
+    // 인터넷 접속하는 매서드
     public void SmartPhoneFn2() {
         System.out.println("스마트 폰으로는 웹 브라우저에 접속할 수 있습니다.");
         System.out.print("접속할 URL을 입력해주세요: ");
@@ -32,8 +34,8 @@ public class Iphone extends SmartPhone{
         Iphone MyIphone = new Iphone();
         System.out.println("나의 핸드폰 번호는: " + MyIphone.PhoneNum + "입니다.");
         System.out.println("나의 핸드폰 기종은: " + MyIphone.PhoneModel + "입니다.");
-       // MyIphone.SmartPhoneFn1();
-        MyIphone.SmartPhoneFn2();
+       // MyIphone.SmartPhoneFn1(); // 전화 거는 메서드 호출
+        MyIphone.SmartPhoneFn2(); // 인터넷 접속하는 메서드 호출
     }
 
 }
