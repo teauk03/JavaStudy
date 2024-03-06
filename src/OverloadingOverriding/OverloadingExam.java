e60671a7427a99c3c138bc930ec640707b3ef7be
teauk03 <kto031124@gamil.com>
2024-03-06 14:20:27 +0900

    overloading EXAM: SUM()

---
+++ src/OverloadingOverriding/OverloadingExam.java
@@ -0,0 +1,20 @@
+package OverloadingOverriding;
+
+public class OverloadingExam {
+    void sum(int a, int b) {
+        System.out.println("두 가지의 수를 더할 수 있는 메서드의 값은: "+(a+b));
+    }
+    void sum(int a, int b, int c) {
+        System.out.println("세 가지의 수를 더할 수 있는 메서드의 값은: "+(a+b+c));
+    }
+    void sum(double a, double b) {
+        System.out.println("두 가지의 실수를 더할 수 있는 메서드의 값은: "+(a+b));
+    }
+    public static void main(String[] args) {
+        OverloadingExam result = new OverloadingExam();
+        result.sum(10, 20);
+        result.sum(10, 20, 30);
+        result.sum(3.14, 3.14);
+
+    }
+}
