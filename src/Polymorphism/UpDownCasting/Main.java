d841729f8b93b85f977079885974691684cc4c60
teauk03 <kto031124@gamil.com>
2024-04-02 12:14:50 +0900

    UP, Down Exam Code

---
+++ src/Polymorphism/UpDownCasting/Main.java
@@ -0,0 +1,31 @@
+package Polymorphism.UpDownCasting;
+class Phone {
+    protected void 전화걸기기능(String name) {
+        System.out.println(name+"은(는) 전화를 거는 중..");
+    }
+}
+class IPhone extends Phone{
+    protected void 인터넷접속기능(String name) {
+        System.out.println(name + "는 인터넷에 접속할 수 있다.");
+    }
+    protected void 애플기기끼리연동기능(String name){
+        System.out.println(name + "는 애플 기기 끼리 연동 됩니다.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Phone phone01 = new IPhone(); // 업 캐스팅
+        phone01.전화걸기기능("폰"); // 가능
+        // phone01.인터넷접속기능 // 불가능
+        // phone01.애플기기끼리연동기능 // 불가능
+
+        // 다운 캐스팅
+        IPhone IPhone11ProMax = (IPhone) phone01;
+        IPhone11ProMax.전화걸기기능("아이폰 11 프로 맥스 ");
+        IPhone11ProMax.애플기기끼리연동기능("아이폰 11 프로 맥스 ");
+        IPhone11ProMax.인터넷접속기능("아이폰 11 프로 맥스");
+
+
+    }
+}
