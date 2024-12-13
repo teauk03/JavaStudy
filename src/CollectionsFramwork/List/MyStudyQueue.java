a04554299998e612fc143cd8ebc4f9d364b8fa5f
teauk03 <kto031124@gamil.com>
2024-12-13 13:07:44 +0900

    학습용 Queue 구현

---
+++ src/CollectionsFramwork/List/MyStudyQueue.java
@@ -0,0 +1,26 @@
+package CollectionsFramwork.List;
+
+
+import java.util.*;
+
+public class MyStudyQueue {
+    private LinkedList list = new LinkedList();
+    // FIFO 방식으로 요소 삽입
+    public void offer(Object item){
+        list.addLast(item);
+    }
+    // FIFO 방식으로 요소 추출
+    public Object poll() {
+        if(list.isEmpty()) {
+            return null;
+        }
+        return list.removeFirst();
+    }
+    // 첫번째 요소 확인
+    public Object peek() {
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.getFirst();
+    }
+}
