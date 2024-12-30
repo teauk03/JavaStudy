dcb0961f567d82c93eb82d781908b0bf2bfc0135
teauk03 <kto031124@gamil.com>
2024-12-30 09:25:59 +0900

    Lambda 예시 코드

---
+++ src/Lambda/LambdaEx01.java
@@ -0,0 +1,41 @@
+package Lambda;
+
+
+@FunctionalInterface
+interface MyFunction {
+    // public abstract void run();
+    void run();
+}
+
+public class LambdaEx01 {
+    static void execute(MyFunction f) {
+        f.run();
+    }
+    static MyFunction getMyFunction() {
+        MyFunction f =() -> System.out.println("f3.run()");
+        return f;
+    }
+
+    public static void main(String[] args) {
+        // 람다식으로 MyFunction run() 구현
+        MyFunction f1 = ()-> System.out.println("f1.run()");
+        // 익명 클래스로 run()을 구현
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2.run()");
+            }
+        };
+
+        MyFunction f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f3);
+        execute( ()->System.out.println("run: "));
+
+
+    }
+}
---
+++ src/Lambda/LambdaEx02.java
@@ -0,0 +1,37 @@
+package Lambda;
+
+// interface MyFcn {
+//  void myMethode;
+//}
+
+
+class Outher {
+    int val = 10;
+
+    class Inner {
+        int val = 20;
+        void method(int i) {  // i=100
+            {
+                int val = 30; // final int val = 30;
+                // i = 10; // -- 상수 i의 값을 변경할 수 없음
+                MyFcn f = () -> {
+                    System.out.println("i: " + i); // 100
+                    System.out.println("val: " + val); //  method 안에 있는 지역변수 val 출력
+                    System.out.println("this.val: " + this.val); // Outher.Inner 클래스의 인스턴스 변수 val 출력
+                    System.out.println("Other.this.val: " + Outher.this.val); // Outher 클래스의 인스턴스 변수 val 출력
+                };
+                f.myMethode();
+            }
+        }
+    }
+}
+
+public class LambdaEx02 {
+    public static void main(String[] args) {
+        Outher outher = new Outher();
+        Outher.Inner inner = outher.new Inner();
+        inner.method(100);
+    }
+
+}
+
